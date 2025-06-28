986c6273b8ed8ac40c31d147b456efa6e24c7d4b
Oliviapw93 <olivia.putri.winata13@gmail.com>
2025-06-29 01:21:09 +0700

    UI olip

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+import random
 
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("home.html")
+app.secret_key = 'your_secret_key_here'  # penting untuk session
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def hero():
+    return render_template('hero.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    
+    year = request.form.get('year')
+    km_driven = request.form.get('km_driven')
+    fuel = request.form.get('fuel')
+    seller_type = request.form.get('seller_type')
+    transmission = request.form.get('transmission')
+    owner = request.form.get('owner')
+
+   
+    predicted_price = random.randint(200000, 2000000)
+
+    session['predicted_price'] = predicted_price
+    return redirect(url_for('result'))
+
+@app.route('/result')
+def result():
+    price = session.get('predicted_price', None)
+    return render_template('result.html', price=price)
+
+if __name__ == '__main__':
+    app.run(debug=True)
